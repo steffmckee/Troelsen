0e7d48711aec701c1eb40c246b25f9b6aa4fa725
Steff McKee <steffmckee@outlook.com>
2017-05-26 13:06:23 -0600

    added UseGenericList() method

--- FunWithGenericCollections/FunWithGenericCollections/Program.cs
+++ FunWithGenericCollections/FunWithGenericCollections/Program.cs
@@ -10,6 +10,39 @@ namespace FunWithGenericCollections
     {
         static void Main(string[] args)
         {
+            UseGenericList();
+
+            Console.ReadLine();
+        }
+
+        static void UseGenericList()
+        {
+            // Make a List of Person objects, filled with
+            // collections/object init syntax.
+            List<Person> people = new List<Person>()
+            {
+                new Person {FirstName="Homer", LastName="Simpson", Age=47},
+                new Person {FirstName="Marge", LastName="Simpson", Age=45},
+                new Person {FirstName="Lisa", LastName="Simpson", Age=9},
+                new Person {FirstName="Bart", LastName="Simpson", Age=8}
+            };
+
+            Console.WriteLine("Items in list: {0}", people.Count);
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("\n->Inserting new person.");
+            people.Insert(2, new Person { FirstName = "Maggie", LastName = "Simpson", Age = 2 });
+            Console.WriteLine("Items in list: {0}", people.Count);
+
+            Person[] arrayOfPeople = people.ToArray();
+            for (int i = 0; i < arrayOfPeople.Length; i++)
+            {
+                Console.WriteLine("First Names: {0}", arrayOfPeople[i].FirstName);
+            }
         }
     }
 }
